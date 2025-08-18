9e316ccfd0854c7bf2d58280c1cbdfa3e5af7d37
AndreWeber_Dev <andrezindsytb@gmail.com>
2025-08-18 20:53:11 -0300

    sla

--- aula04/varLetConst.js
+++ aula04/varLetConst.js
@@ -1,9 +1,9 @@
 var n1 = 3;
 var n2 = 20;
 let n3 = 100;
-n3 = 200; 
+n3 = 200;
 const list = ["José, Maria", "João", "Ana"];
-console.log(list);  
+console.log(list);
 console.log(n1 + n2);
 
 if (true) {
@@ -13,3 +13,7 @@ if (true) {
   console.log(n1);
   console.log(n2);
 }
+let b = 10
+let h = 25
+let a = b * h;
+console.log(a);
