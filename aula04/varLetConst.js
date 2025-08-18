dd937585b50354f45c9478b1c1810eb079a1d438
AndreWeber_Dev <andrezindsytb@gmail.com>
2025-08-18 20:20:43 -0300

    update varletconst.js

--- aula04/varLetConst.js
+++ aula04/varLetConst.js
@@ -1,4 +1,15 @@
 var n1 = 3;
 var n2 = 20;
-
+let n3 = 100;
+n3 = 200; 
+const list = ["José, Maria", "João", "Ana"];
+console.log(list);  
 console.log(n1 + n2);
+
+if (true) {
+  var n1 = 50;
+  //LET N2 = 25
+  n2 = 25;
+  console.log(n1);
+  console.log(n2);
+}
